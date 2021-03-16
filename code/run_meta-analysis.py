0e2bc83abb89b533e2e88392aaacde0526dffe31
Michael Riedel <miriedel@fiu.edu>
2021-03-16 17:29:15 -0400

    included conjunction analysis and last study in spreadsheet that was cut off

--- code/run_meta-analysis.py
+++ code/run_meta-analysis.py
@@ -8,6 +8,19 @@ import pandas as pd
 from atlasreader import atlasreader
 
 
+def conjunction(img1_fn, img2_fn, output_dir):
+    img1 = nib.load(img1_fn)
+    img2 = nib.load(img2_fn)
+    inds1 = np.ravel_multi_index(np.nonzero(img1.get_fdata()), img1.shape)
+    inds2 = np.ravel_multi_index(np.nonzero(img2.get_fdata()), img2.shape)
+    inds12 = np.unravel_index(np.intersect1d(inds1, inds2), img1.shape)
+    minstat = np.min(np.vstack((img1.get_fdata()[inds12], img2.get_fdata()[inds12])), axis=0)
+
+    conjunction_img = np.zeros(img1.shape)
+    conjunction_img[inds12] = minstat
+    os.makedirs(output_dir, exist_ok=True)
+    nib.save(nib.Nifti1Image(conjunction_img, img1.affine), op.join(output_dir, 'conjunction.nii.gz'))
+
 #define some functions
 def thresh_img(logp_img, z_img, p):
     sig_inds = np.where(logp_img.get_fdata() > -np.log(p))
@@ -43,44 +56,51 @@ def run_subtraction(dset1, dset2, output_dir, prefix1, prefix2):
     alesubtraction = nimare.meta.ALESubtraction(n_iters=10000)
 
     #calculate ale statistics
-    sres = alesubtraction.fit(dset1, dset2)
+    alesubtraction.fit(dset1, dset2)
+    alesubtraction.results.maps['p'] = alesubtraction.results.maps['p_desc-group1MinusGroup2']
+
+    #sres.save_maps(output_dir=op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2)), prefix='{}_gt_{}'.format(prefix1, prefix2))
+    #run FDR multiple comparisons correction
+    corr = nimare.correct.FDRCorrector(method="indep")
+    cres = corr.transform(alesubtraction.results)
 
     #save output maps and dataset
     os.makedirs(output_dir, exist_ok=True)
-    sres.save_maps(output_dir=op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2)), prefix='{}_gt_{}'.format(prefix1, prefix2))
+    cres.save_maps(output_dir=op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2)), prefix='{}_gt_{}'.format(prefix1, prefix2))
 
-    #get contrast analysis results
-    z_img = nib.load(op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2), '{}_gt_{}_z_desc-group1MinusGroup2.nii.gz'.format(prefix1, prefix2)))
+    if 1 == 0:
+        #get contrast analysis results
+        z_img = nib.load(op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2), '{}_gt_{}_z_desc-group1MinusGroup2.nii.gz'.format(prefix1, prefix2)))
 
-    #threshold each direction at p < 0.05
-    #get dset1 info
-    z_img_group1 = nib.load(op.join(output_dir, prefix1, '{}_z.nii.gz'.format(prefix1)))
-    z_img_group1_data = z_img_group1.get_fdata()
-    #get dset2 info
-    z_img_group2 = nib.load(op.join(output_dir, prefix2, '{}_z.nii.gz'.format(prefix2)))
-    z_img_group2_data = z_img_group2.get_fdata()
+        #threshold each direction at p < 0.05
+        #get dset1 info
+        z_img_group1 = nib.load(op.join(output_dir, prefix1, '{}_z.nii.gz'.format(prefix1)))
+        z_img_group1_data = z_img_group1.get_fdata()
+        #get dset2 info
+        z_img_group2 = nib.load(op.join(output_dir, prefix2, '{}_z.nii.gz'.format(prefix2)))
+        z_img_group2_data = z_img_group2.get_fdata()
 
-    #for first direction
-    sig_inds = np.where(z_img.get_fdata() > -np.log(0.05))
+        #for first direction
+        sig_inds = np.where(z_img.get_fdata() > -np.log(0.05))
 
-    zimg1_sub_zimg2 = z_img_group1_data - z_img_group2_data
+        zimg1_sub_zimg2 = z_img_group1_data - z_img_group2_data
 
-    z_img_group1_thresh_data = np.zeros(z_img_group1.shape)
-    z_img_group1_thresh_data[sig_inds] = zimg1_sub_zimg2[sig_inds]
+        z_img_group1_thresh_data = np.zeros(z_img_group1.shape)
+        z_img_group1_thresh_data[sig_inds] = zimg1_sub_zimg2[sig_inds]
 
-    z_img_group1_final = nib.Nifti1Image(z_img_group1_thresh_data, z_img_group1.affine)
-    nib.save(z_img_group1_final, op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2), '{}-{}_z_thresh-05.nii.gz'.format(prefix1, prefix2)))
+        z_img_group1_final = nib.Nifti1Image(z_img_group1_thresh_data, z_img_group1.affine)
+        nib.save(z_img_group1_final, op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2), '{}-{}_z_thresh-05.nii.gz'.format(prefix1, prefix2)))
 
-    #for second direction
-    sig_inds = np.where(z_img.get_fdata() < np.log(0.05))
+        #for second direction
+        sig_inds = np.where(z_img.get_fdata() < np.log(0.05))
 
-    zimg2_sub_zimg1 = z_img_group2_data - z_img_group1_data
+        zimg2_sub_zimg1 = z_img_group2_data - z_img_group1_data
 
-    z_img_group2_thresh_data = np.zeros(z_img_group2.shape)
-    z_img_group2_thresh_data[sig_inds] = zimg2_sub_zimg1[sig_inds]
+        z_img_group2_thresh_data = np.zeros(z_img_group2.shape)
+        z_img_group2_thresh_data[sig_inds] = zimg2_sub_zimg1[sig_inds]
 
-    z_img_group2_final = nib.Nifti1Image(z_img_group2_thresh_data, z_img_group2.affine)
-    nib.save(z_img_group2_final, op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2), '{}-{}_z_thresh-05.nii.gz'.format(prefix2, prefix1)))
+        z_img_group2_final = nib.Nifti1Image(z_img_group2_thresh_data, z_img_group2.affine)
+        nib.save(z_img_group2_final, op.join(output_dir, '{}_gt_{}'.format(prefix1, prefix2), '{}-{}_z_thresh-05.nii.gz'.format(prefix2, prefix1)))
 
 def dataset2table(dset, output_dir, prefix):
     dset.metadata.to_csv(op.join(output_dir, '{}.csv'.format(prefix)), sep=',', index=False)
@@ -146,11 +166,12 @@ grammatical_dset = dset_include.slice(grammatical_idx)
 
 #define output directory and run ale function defined above
 output_dir = op.join(project_directory, 'derivatives', 'grammatical')
-run_ale(grammatical_dset, output_dir, 'grammatical')
+#run_ale(grammatical_dset, output_dir, 'grammatical')
 #save the dataset
 grammatical_dset.save(op.join(output_dir, "grammatical.pkl"))
 dataset2table(grammatical_dset, output_dir, 'grammatical')
-get_peaks(op.join(output_dir, '{}_z_corr-FWE_thresh-05.nii.gz'.format('grammatical')), output_dir)
+grammatical_fn = op.join(output_dir, '{}_z_corr-FWE_thresh-05.nii.gz'.format('grammatical'))
+get_peaks(grammatical_fn, output_dir)
 
 #select the ungrammatical contrasts
 ungrammatical_idx = dset_include.annotations['id'][dset_include.annotations['Meta-Analysis Group'] == 'Ungrammatical']
@@ -158,12 +179,18 @@ ungrammatical_dset = dset_include.slice(ungrammatical_idx)
 
 #define output directory and run ale function defined above
 output_dir = op.join(project_directory, 'derivatives', 'ungrammatical')
-run_ale(ungrammatical_dset, output_dir, 'ungrammatical')
+#un_ale(ungrammatical_dset, output_dir, 'ungrammatical')
 #save the dataset
 ungrammatical_dset.save(op.join(output_dir, "ungrammatical.pkl"))
 dataset2table(ungrammatical_dset, output_dir, 'ungrammatical')
-get_peaks(op.join(output_dir, '{}_z_corr-FWE_thresh-05.nii.gz'.format('ungrammatical')), output_dir)
+ungrammatical_fn = op.join(output_dir, '{}_z_corr-FWE_thresh-05.nii.gz'.format('ungrammatical'))
+get_peaks(ungrammatical_fn, output_dir)
 
+output_dir = op.join(project_directory, 'derivatives', 'conjunction')
+conjunction_fn = op.join(output_dir, 'conjunction.nii.gz')
+conjunction(grammatical_fn, ungrammatical_fn, output_dir)
+get_peaks(conjunction_fn, output_dir)
+exit()
 #run subtraction analysis
 output_dir = op.join(project_directory, 'derivatives')
 run_subtraction(grammatical_dset, ungrammatical_dset, output_dir, 'grammatical', 'ungrammatical')

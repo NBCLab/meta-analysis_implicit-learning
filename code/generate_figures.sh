b9c138b3990d836c63c581d5e1a313d7bc200868
Michael Riedel <miriedel@fiu.edu>
2021-03-29 07:18:15 -0400

    performed follow up analysis on pooled meta-analysis, altered results organization accordingly

--- code/generate_figures.sh
+++ code/generate_figures.sh
@@ -25,57 +25,63 @@ python3 /Users/miriedel/Desktop/GitHub/surflay/make_figures.py \
   --colormap Oranges \
   --opacity 0.75
 
-if [ 1 -eq 0 ]; then
-  #make figures for rsfc and macm maps
-  rois=$(ls $project_directory/derivatives/rois/)
+#pooled
+python3 /Users/miriedel/Desktop/GitHub/surflay/make_figures.py \
+  -f $project_directory/derivatives/pooled/pooled_z_corr-FWE-cluster_thresh-05.nii.gz \
+  -o $project_directory/derivatives/figures/ \
+  -z 48 38 20 -2 \
+  --colormap Greens \
+  --opacity 0.75
 
-  for roi in $rois; do
-    roi_prefix=${roi%.nii.gz*}
-    echo $roi_prefix
+#make figures for rsfc and macm maps
+rois=$(ls $project_directory/derivatives/pooled_analysis/rois/)
 
-    x=${roi_prefix%%_*}
-    y=${roi_prefix%_*}
-    y=${y#*_}
-    z=${roi_prefix##*_}
+for roi in $rois; do
+  roi_prefix=${roi%.nii.gz*}
+  echo $roi_prefix
 
-    #rsfc
-    if [ ! -d $project_directory/derivatives/figures/rsfc ]; then
-      mkdir -p $project_directory/derivatives/figures/rsfc
-    fi
-    python3 /Users/miriedel/Desktop/GitHub/surflay/make_slice_figures.py \
-      -f $project_directory/derivatives/rsfc/"$x"_"$y"_"$z"_tstat1_thr001.nii.gz \
-      --roi_file $project_directory/derivatives/rois/"$x"_"$y"_"$z".nii.gz \
-      -x ${x%%.*} \
-      -y ${y%%.*} \
-      -z ${z%%.*} \
-      --outdir $project_directory/derivatives/figures/rsfc \
-      --colormap hsv
+  x=${roi_prefix%%_*}
+  y=${roi_prefix%_*}
+  y=${y#*_}
+  z=${roi_prefix##*_}
 
-    #macm
-    if [ ! -d $project_directory/derivatives/figures/macm ]; then
-      mkdir -p $project_directory/derivatives/figures/macm
-    fi
-    python3 /Users/miriedel/Desktop/GitHub/surflay/make_slice_figures.py \
-      -f $project_directory/derivatives/macm/"$x"_"$y"_"$z"/"$x"_"$y"_"$z"_z_corr-FWE-voxel_thresh-0.001.nii.gz \
-      --roi_file $project_directory/derivatives/rois/"$x"_"$y"_"$z".nii.gz \
-      -x ${x%%.*} \
-      -y ${y%%.*} \
-      -z ${z%%.*} \
-      --outdir $project_directory/derivatives/figures/macm \
-      --colormap hsv
+  #rsfc
+  if [ ! -d $project_directory/derivatives/figures/pooled_analysis/rsfc ]; then
+    mkdir -p $project_directory/derivatives/figures/pooled_analysis/rsfc
+  fi
+  python3 /Users/miriedel/Desktop/GitHub/surflay/make_slice_figures.py \
+    -f $project_directory/derivatives/pooled_analysis/rsfc/"$x"_"$y"_"$z"_tstat1_thr001.nii.gz \
+    --roi_file $project_directory/derivatives/pooled_analysis/rois/"$x"_"$y"_"$z".nii.gz \
+    -x ${x%%.*} \
+    -y ${y%%.*} \
+    -z ${z%%.*} \
+    --outdir $project_directory/derivatives/figures/pooled_analysis/rsfc \
+    --colormap hsv
 
-    #conjunction connectivity
-    if [ ! -d $project_directory/derivatives/figures/connectivity ]; then
-      mkdir -p $project_directory/derivatives/figures/connectivity
-    fi
-    python3 /Users/miriedel/Desktop/GitHub/surflay/make_slice_figures.py \
-      -f $project_directory/derivatives/connectivity/"$x"_"$y"_"$z"/conjunction.nii.gz \
-      --roi_file $project_directory/derivatives/rois/"$x"_"$y"_"$z".nii.gz \
-      -x ${x%%.*} \
-      -y ${y%%.*} \
-      -z ${z%%.*} \
-      --outdir $project_directory/derivatives/figures/connectivity \
-      --colormap hsv
+  #macm
+  if [ ! -d $project_directory/derivatives/figures/pooled_analysis/macm ]; then
+    mkdir -p $project_directory/derivatives/figures/pooled_analysis/macm
+  fi
+  python3 /Users/miriedel/Desktop/GitHub/surflay/make_slice_figures.py \
+    -f $project_directory/derivatives/pooled_analysis/macm/"$x"_"$y"_"$z"/"$x"_"$y"_"$z"_z_corr-FWE-voxel_thresh-0.001.nii.gz \
+    --roi_file $project_directory/derivatives/pooled_analysis/rois/"$x"_"$y"_"$z".nii.gz \
+    -x ${x%%.*} \
+    -y ${y%%.*} \
+    -z ${z%%.*} \
+    --outdir $project_directory/derivatives/figures/pooled_analysis/macm \
+    --colormap hsv
 
-    done
-fi
+  #conjunction connectivity
+  if [ ! -d $project_directory/derivatives/figures/pooled_analysis/connectivity ]; then
+    mkdir -p $project_directory/derivatives/figures/pooled_analysis/connectivity
+  fi
+  python3 /Users/miriedel/Desktop/GitHub/surflay/make_slice_figures.py \
+    -f $project_directory/derivatives/pooled_analysis/connectivity/"$x"_"$y"_"$z"/conjunction.nii.gz \
+    --roi_file $project_directory/derivatives/pooled_analysis/rois/"$x"_"$y"_"$z".nii.gz \
+    -x ${x%%.*} \
+    -y ${y%%.*} \
+    -z ${z%%.*} \
+    --outdir $project_directory/derivatives/figures/pooled_analysis/connectivity \
+    --colormap hsv
+
+  done
